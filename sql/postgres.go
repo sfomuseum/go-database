fef0c35c57b4a7b3aac829b53a2a2a05b3d484a2
thisisaaronland <thisisaaronland@localhost>
2024-12-14 11:47:09 -0800

    postgres wranglign

--- sql/postgres.go
+++ sql/postgres.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	_ "log/slog"
+	"log/slog"
 )
 
 func ConfigurePostgresDatabase(ctx context.Context, db *sql.DB, opts *ConfigureDatabaseOptions) error {
@@ -13,6 +13,9 @@ func ConfigurePostgresDatabase(ctx context.Context, db *sql.DB, opts *ConfigureD
 
 		for _, t := range opts.Tables {
 
+			logger := slog.Default()
+			logger = logger.With("table", t.Name())
+			
 			exists, err := HasPostgresTable(ctx, db, t.Name())
 
 			if err != nil {
@@ -20,6 +23,7 @@ func ConfigurePostgresDatabase(ctx context.Context, db *sql.DB, opts *ConfigureD
 			}
 
 			if exists {
+				logger.Debug("Table already exists")
 				continue
 			}
 
@@ -34,6 +38,8 @@ func ConfigurePostgresDatabase(ctx context.Context, db *sql.DB, opts *ConfigureD
 			if err != nil {
 				return fmt.Errorf("Failed to create %s table, %w", t.Name(), err)
 			}
+
+			logger.Debug("Created table")
 		}
 	}
 
@@ -44,6 +50,9 @@ func ConfigurePostgresDatabase(ctx context.Context, db *sql.DB, opts *ConfigureD
 
 func HasPostgresTable(ctx context.Context, db *sql.DB, table_name string) (bool, error) {
 
+	logger := slog.Default()
+	logger = logger.With("table", table_name)
+	
 	q := "SELECT EXISTS(SELECT * FROM pg_tables WHERE schemaname='public' AND tablename=$1)"
 	
 	row := db.QueryRowContext(ctx, q, table_name)
@@ -56,5 +65,6 @@ func HasPostgresTable(ctx context.Context, db *sql.DB, table_name string) (bool,
 		return false, fmt.Errorf("Failed to query table, %w", err)
 	}
 
+	logger.Debug("Does table exist", "exists", exists)
 	return exists, nil
 }
